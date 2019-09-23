5299563e251ec67a11f2cd2391f64f02de1f23d8
Isaac <ivazquez@ipcmobile.com>
2019-09-22 21:40:17 -0700

    finalized content minus hero image

--- Linea Pro 5/dev/js/script.js
+++ Linea Pro 5/dev/js/script.js
@@ -1,21 +1,21 @@
 document.addEventListener('DOMContentLoaded', function () {
     let actionBar = document.getElementsByClassName('action-bar')[0];
-    let containerBar = actionBar.firstElementChild;
+    let actionFloat = actionBar.firstElementChild;
     var actionOrigin = actionBar.offsetTop;
-    let wrapper = document.createElement('div');
+    // let wrapper = document.createElement('div');
 
     window.onscroll = function () {
         let currentScroll = document.documentElement.scrollTop || document.body.scrollTop; // Get Current Scroll Value
         if (actionBar.offsetTop  <= currentScroll) {
             
-            actionBar.replaceChild(wrapper, containerBar);
-            wrapper.appendChild(containerBar);
-            wrapper.classList.add('active');
-        }else if(currentScroll - 75 <= actionOrigin){
-            wrapper.classList.remove('active');
-            // actionBar.replaceChild(containerBar, wrapper);
-            actionBar.appendChild(containerBar);
-            wrapper.remove();
+            // actionBar.replaceChild(wrapper, actionFloat);
+            // wrapper.appendChild(actionFloat);
+            actionFloat.classList.add('active');
+        }else if(currentScroll  <= actionOrigin){
+            actionFloat.classList.remove('active');
+            // // actionBar.replaceChild(actionFloat, wrapper);
+            // actionBar.appendChild(actionFloat);
+            // wrapper.remove();
         }
      
         
